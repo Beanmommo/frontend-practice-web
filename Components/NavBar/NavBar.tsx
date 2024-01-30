773dcf5932c64bfb5f2d4e1cb19791a5d444c60b
Beanmommo <leonardo.prasetyo5@gmail.com>
2024-01-30 14:40:44 +1100

    Added NavBar component and landing page content for main page

---
+++ Components/NavBar/NavBar.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./styles.css"
+
+const NavBar = () => {
+  return (
+    <nav className='nav'>
+        <ul>
+            <li>
+                <a href='/'>Home</a>
+            </li>
+            <li>
+                <a href='/'>Features</a>
+            </li>
+            <li>
+                <a href='/'>Pricing</a>
+            </li>
+            <li>
+                <a href='/'>Blog</a>
+            </li>
+            <li>
+                <a href='/'>Logo</a>
+            </li>
+            <li>
+                <a href='/'>Be</a>
+            </li>
+        </ul>
+        
+    </nav>
+  )
+}
+5
+export default NavBar
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
+import NavBar from "@/Components/NavBar/NavBar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-      <h1>Hello World</h1>
+      <NavBar></NavBar>
+
+      <h4>Startup 3</h4>
+      <h1>Forget About Code</h1>
+      <p>Startup Framework gives you complete freedom over your creative process - you dont have to think about any technical aspects. There are no limits and absolutely no coding
+      </p>
+      
+      <div>Carousel bullets</div>
+
+      <button>Create an account</button>
+
     </main>
   );
 }
