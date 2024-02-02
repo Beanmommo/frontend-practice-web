9a0724df89f9fd7a3fdfa46931028d6f1b1adb37
Beanmommo <leonardo.prasetyo5@gmail.com>
2024-02-02 22:23:31 +1100

    Moved CarouselTab Css properties into one file

--- Components/CarouselTab.tsx
+++ Components/CarouselTab.tsx
@@ -1,12 +1,45 @@
-import "./Styles/carouseltab.css"
+import CSS from 'csstype'
+
+const carouseltab: CSS.Properties = {
+  display: 'grid',
+  justifyContent: 'center'
+}
+
+const h3styles: CSS.Properties = {
+  display: 'flex',
+  justifyContent: 'center',
+  fontSize: '24px',
+  lineHeight: '31.25px',
+  fontWeight: '700',
+  paddingBottom: '2rem'
+}
+
+const h1styles: CSS.Properties = {
+  display: 'flex',
+  justifyContent: 'center',
+  fontSize: '72px',
+  lineHeight: '86px',
+  fontWeight:'700',
+  paddingBottom: '3rem'
+}
+
+const h5styles: CSS.Properties = {
+  display: 'flex',
+  justifyContent: 'center',
+  fontSize: '22px',
+  lineHeight: '32px',
+  wordWrap: 'normal',
+  width: '736px',
+  textAlign: 'center'
+}
 
 export const CarouselTab = () => {
   return (
 
-    <div className='carousel-tab'>
-        <h3>Startup 3</h3>
-        <h1>Forget About Code</h1>
-        <h5>
+    <div style={carouseltab}>
+        <h3 style={h3styles}>Startup 3</h3>
+        <h1 style={h1styles}>Forget About Code</h1>
+        <h5 style={h5styles}>
             Startup Framework gives you complete freedom over your creative process - you dont have to think about any technical aspects. There are no limits and absolutely no coding
         </h5>
     </div>
@@ -14,3 +47,4 @@ export const CarouselTab = () => {
   )
 }
 export default CarouselTab;
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 
 import Content from "@/pages/Content";
 import LandingPage from "@/pages/LandingPage";
-import Image from "next/image";
 
 export default function Home() {
   return (
